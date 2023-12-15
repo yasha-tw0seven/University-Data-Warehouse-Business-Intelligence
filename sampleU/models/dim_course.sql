f17ba7c98ece25db13529c04e2a8998de005fc2b
databusters57@gmail.com <databusters57@gmail.com>
2023-12-15 22:33:20 +0000

    Commit and Sync

--- sampleU/models/dim_course.sql
+++ sampleU/models/dim_course.sql
@@ -1,8 +1,4 @@
+{{ config(materialized="table") }}
 with stg_course as (select * from {{ source("SAMPLEU", "COURSE") }})
-select
-    {{ dbt_utils.generate_surrogate_key(["stg_course.CourseID"]) }} as coursekey,
-    courseid,
-    departmentid,
-    title,
-    credits
+select courseid, departmentid, title, credits
 from stg_course
